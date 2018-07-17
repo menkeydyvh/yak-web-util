f782c08386834f51e68b483064575b27cd0e8a5e
xhyu <xhyu@1ping.com>
2018-07-17 14:04:29 +0800

    工具类调整

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
  * Created by menkey on 2018/1/25.
  */
 import util from './src/util';
+import vueExpand from './src/util/vueExpand'
 
 import frameApi from './src/frame/frameApi';
 import vueReceiveParentToMessages from './src/frame/vueReceiveParentToMessages';
 
 export {
   util,
+  vueExpand,
   frameApi,
   vueReceiveParentToMessages
 }
