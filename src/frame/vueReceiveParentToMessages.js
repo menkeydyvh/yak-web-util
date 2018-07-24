d7c6795128d032b1c84db3607623d6be3ec2e266
xhyu <xhyu@1ping.com>
2018-07-24 10:24:34 +0800

    工具类调整发布npm

--- src/frame/vueReceiveParentToMessages.js
+++ src/frame/vueReceiveParentToMessages.js
@@ -3,39 +3,41 @@ import util from '../util';
 export default (callback) => {
   if (process.env.NODE_ENV === 'production') {
     window.addEventListener("message", (event) => {
-      if (event.origin === process.env.HOST_HTTP) {
-        let json = event.data;
-        if (typeof event.data === 'string') {
-          json = JSON.parse(event.data);
-        }
-        switch (json.type) {
-          case 'localStorage': {
-            for (let key in json.data) {
-              util.lockr(key, json.data[key]);
-            }
-            break;
-          }
-          case 'historyListen': {
-            if (json.data === 'forward') {
-              //前进
-              window.bus.$router.go(1);
-            } else if (json.data === 'back') {
-              //后退
-              window.bus.$router.go(-1);
-            }
-            break;
-          }
-          default: {
-            break;
-          }
-        }
-        callback && callback();
-      } else {
-        console.error('来源不可信');
+      let json = event.data;
+      if (typeof event.data === 'string') {
+        json = JSON.parse(event.data);
       }
+      switch (json.type) {
+        case 'localStorage': {
+          for (let key in json.data) {
+            util.lockr(key, json.data[key]);
+          }
+          break;
+        }
+        case 'historyListen': {
+          if (json.data === 'forward') {
+            //前进
+            window.bus.$router.go(1);
+          } else if (json.data === 'back') {
+            //后退
+            window.bus.$router.go(-1);
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+      callback && callback();
+      //  先不处理来源可信度
+      // if (event.origin === process.env.HOST_HTTP) {
+      // } else {
+      //   console.error('来源不可信');
+      // }
     }, false);
   } else {
-    util.lockr('uid',1);
+    //测试的时候随便设置的
+    util.lockr('uid', 1);
     callback && callback();
   }
 };
